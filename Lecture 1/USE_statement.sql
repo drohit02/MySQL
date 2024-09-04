5ee8e407b692f9eca9bab6b7f08019cdfc71d927
drohit02 <drohit045@gmail.com>
2024-09-05 00:02:53 +0530

    USE statement in SQL

---
+++ Lecture 1/USE_statement.sql
@@ -0,0 +1,8 @@
+/* USE statement : 
+1. The SQL USE statement is used to select a database from a list of databases available in the system
+2. Once the database is selected,we can perform various operations on it such as creating tables,inserting data
+	updating data and deleting data
+3. Syntax : USE <DatabaseName>;
+ */
+ USE planetarygoods;
+ SELECT * FROM orders;

7a4817904475ff8a8749cb38bf02d1b916e0fda9
drohit02 <drohit045@gmail.com>
2024-09-09 23:32:41 +0530

    More SELECT query

---
+++ Lecture 1/Adv_SELECT_Query.sql
@@ -0,0 +1,4 @@
+use planetarygoods;
+/*Que 1 : The Inventory Department going to review on our products. But they need all the data related to proyducts
+           so write a query to get all the produts*/
+SELECT * FROM products;

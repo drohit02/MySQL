50d00ef51a6d6e7247239e40b3b2d76dc87c5140
drohit02 <drohit045@gmail.com>
2024-09-11 23:47:17 +0530

    distinct keyword use

--- Lecture 1/Adv_SELECT_Query.sql
+++ Lecture 1/Adv_SELECT_Query.sql
@@ -1,4 +1,8 @@
 use planetarygoods;
-/*Que 1 : The Inventory Department going to review on our products. But they need all the data related to proyducts
+/*Que 1 : The Inventory Department going to review on our products. But they need all the data related to products
            so write a query to get all the produts*/
 SELECT * FROM products;
+
+/*Que 2.Write a query to get City name.City should not be repeated */
+SELECT distinct City
+FROM customers;
